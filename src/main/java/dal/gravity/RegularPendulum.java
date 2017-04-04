18904f4050e3b6a451ca77e444a96dce9a748aa1
Braden <Braden@Bradens_PC>
2017-04-04 14:51:16 -0300

    Made changes to remove AbstractEarthPendulum redundancy

--- src/main/java/dal/gravity/RegularPendulum.java
+++ src/main/java/dal/gravity/RegularPendulum.java
@@ -3,41 +3,54 @@ package dal.gravity;
 /**
  * Represents a pendulum
  */
-public class RegularPendulum extends AbstractEarthPendulum {
-    private double delta, iterations = 0;
-    private double dissipation;
-    private double lastTheta, lastVel, lastAccel;
+public class RegularPendulum extends AbstractPendulum {
+	private double delta, iterations = 0;
+	private double dissipation;
+	private double lastTheta, lastVel, lastAccel;
+	public final static double GRAVITY = 9.80665;
+	
+	/**
+	 * Creates a new Pendulum instance
+	 */
+	public RegularPendulum(double inLength, double inMass, double inTheta0, double inDelta, double inDiss) {
+		super(inLength, inMass, inTheta0,GRAVITY);
+		delta = inDelta;
+		dissipation = inDiss;
+		lastVel = 0;
+		lastTheta = this.getMaxAngularDisplacement();
+		lastAccel = -(this.getGravitationalField() / this.getStringLength()) * Math.sin(lastTheta);
+	}
 
-    /**
-     * Creates a new Pendulum instance 
-     */
-    public RegularPendulum (double inLength, double inMass, double inTheta0, 
-		     double inDelta, double inDiss) {
-	super (inLength, inMass, inTheta0);
-	delta=inDelta;
-	dissipation = inDiss;
-	lastVel = 0;
-	lastTheta = this.getMaxAngularDisplacement ();
-	lastAccel = -(this.getGravitationalField () / this.getStringLength ())*Math.sin (lastTheta);
-    }
+	public RegularPendulum(double inLength, double inMass, double inTheta0, double inDelta) {
+		this(inLength, inMass, inTheta0, inDelta, 0);
+	}
 
-    public RegularPendulum (double inLength, double inMass, double inTheta0, 
-		     double inDelta) {
-	this (inLength, inMass, inTheta0, inDelta, 0);
-    }
+	public void step() {
+		iterations++;
+		lastTheta = lastTheta + lastVel * delta;
+		lastVel = lastVel + lastAccel * delta;
+		lastAccel = -dissipation * lastVel
+				- this.getGravitationalField() / this.getStringLength() * Math.sin(lastTheta);
+	}
 
-    public void step () {
-	iterations++;
-	lastTheta = lastTheta + lastVel*delta;
-	lastVel = lastVel + lastAccel*delta;
-	lastAccel = - dissipation*lastVel - this.getGravitationalField () / this.getStringLength () * Math.sin (lastTheta);
-    }
+	public double getLastTheta() {
+		return lastTheta;
+	}
 
-    public double getLastTheta () { return lastTheta; }
-    public double getLastVelocity () { return lastVel; }
-    public double getLastAcceleration () { return lastAccel; }
-    public double getLastTime () { return iterations*delta; }
-    public double getDissipationConstant () { return dissipation; }
-    
+	public double getLastVelocity() {
+		return lastVel;
+	}
+
+	public double getLastAcceleration() {
+		return lastAccel;
+	}
+
+	public double getLastTime() {
+		return iterations * delta;
+	}
+
+	public double getDissipationConstant() {
+		return dissipation;
+	}
 
 }
--- src/main/java/dal/gravity/SimplePendulum.java
+++ src/main/java/dal/gravity/SimplePendulum.java
@@ -3,37 +3,40 @@ package dal.gravity;
 /**
  * Represents pendulums exhibiting (approximately) simple harmonic motion
  */
-public class SimplePendulum extends AbstractEarthPendulum {
+public class SimplePendulum extends AbstractPendulum {
 
-    private double angularFrequency, periodOfMotion;
+	private double angularFrequency, periodOfMotion;
+	public final static double GRAVITY = 9.80665;
+	
+	/**
+	 * Creates a new Pendulum instance using inLength: the string length (>0)
+	 * inMass: the point mass (>0) inTheta0: angular displacement at t=0
+	 * (0<=theta0<=pi/6)
+	 */
+	public SimplePendulum(double inLength, double inMass, double inTheta0) {
+		super(inLength, inMass, inTheta0,GRAVITY);
+		angularFrequency = Math.sqrt(this.getGravitationalField() / this.getStringLength());
+		periodOfMotion = 2 * Math.PI * Math.sqrt(this.getStringLength() / this.getGravitationalField());
+	}
 
-    /**
-     * Creates a new Pendulum instance using
-     * inLength: the string length (>0)
-     * inMass: the point mass (>0)
-     * inTheta0: angular displacement at t=0 (0<=theta0<=pi/6)
-     */
-    public SimplePendulum (double inLength, double inMass, double inTheta0) {
-	super (inLength, inMass, inTheta0);
-	angularFrequency = Math.sqrt (this.getGravitationalField () / this.getStringLength ());
-	periodOfMotion = 2 * Math.PI 
-	    * Math.sqrt (this.getStringLength () / this.getGravitationalField ());
-    }
+	/**
+	 * provides this Pendulum's angular frequency
+	 */
+	public double getAngularFrequency() {
+		return angularFrequency;
+	}
 
-    /**
-     * provides this Pendulum's angular frequency
-     */ 
-    public double getAngularFrequency () { return angularFrequency; }
+	/**
+	 * provides this Pendulum's period of motion
+	 */
+	public double getPeriodOfMotion() {
+		return periodOfMotion;
+	}
 
-    /**
-     * provides this Pendulum's period of motion
-     */ 
-    public double getPeriodOfMotion () { return periodOfMotion; }
-
-    /**
-     * provides the angular displacement of this Pendulum at time t
-     */
-    public double getTheta (double t) {
-	return this.getMaxAngularDisplacement () * Math.cos (angularFrequency * t);
-    }
+	/**
+	 * provides the angular displacement of this Pendulum at time t
+	 */
+	public double getTheta(double t) {
+		return this.getMaxAngularDisplacement() * Math.cos(angularFrequency * t);
+	}
 }
